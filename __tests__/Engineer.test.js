3312c00a5b438b881ce696099eff01822e43359a
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-03-04 11:29:08 +0000

    Engineer test pass

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,11 @@
-const Engineer =require('')
+const Engineer =require('../lib/Engineer')
+
+describe('Engineer Class detail', () => {
+    it('creates an obj with properties', () => {
+    const engineer = new Engineer(12345, 'Bob')
+
+    expect(engineer.id).toEqual(12345);
+    expect(engineer.name).toEqual('Bob');
+    
+    });
+});
