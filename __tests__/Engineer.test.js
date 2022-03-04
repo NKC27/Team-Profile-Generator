e0a8907ef1c10a7ef37329a2e18accbe0f7f043e
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-03-04 11:29:37 +0000

    spacing of code

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,4 @@
-const Engineer =require('../lib/Engineer')
+const Engineer = require('../lib/Engineer')
 
 describe('Engineer Class detail', () => {
     it('creates an obj with properties', () => {
