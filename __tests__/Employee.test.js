3571d0439a302b915847316d95409174acc6f25c
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-03-04 11:23:15 +0000

    test initialised

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,9 +3,10 @@ const Employee = require('../lib/Employee');
 
 describe('Employee Class detail', () => {
     it('creates an obj with properties', () => {
-    const employee = new Employee(12345, 'Bob')
+    const employee = new Employee(12345, 'Bob', 'mail@mail.com')
 
     expect(employee.id).toEqual(12345);
     expect(employee.name).toEqual('Bob');
+    expect(employee.email).toEqual('mail@mail.com');
     });
 });
