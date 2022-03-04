5cb3cd1e026c248049d3e4fb8dca6c5a6077a34c
NKC27 <93439579+NKC27@users.noreply.github.com>
2022-03-04 10:57:35 +0000

    Employee.test npm test-pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,11 @@
-const Employee = require('');
+const { expect } = require('@jest/globals');
+const Employee = require('../lib/Employee');
+
+describe('Employee Class detail', () => {
+    it('creates an obj with properties', () => {
+    const employee = new Employee(12345, 'Bob')
+
+    expect(employee.id).toEqual(12345);
+    expect(employee.name).toEqual('Bob');
+    });
+});
